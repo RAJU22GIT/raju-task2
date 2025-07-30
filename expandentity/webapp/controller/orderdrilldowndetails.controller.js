1c5646ffcd6dfe6bb547feeff1d8eceb02dae898
RAJU R P <raju.ravichandran@laderatechnology.com>
2025-07-30 10:27:52 +0000

    commit

--- expandentity/webapp/controller/orderdrilldowndetails.controller.js
+++ expandentity/webapp/controller/orderdrilldowndetails.controller.js
@@ -10,13 +10,18 @@ sap.ui.define([
             sap.ui.core.UIComponent.getRouterFor(this).getRoute("RouteOrderDetails1").attachPatternMatched(this._objPatternMatched,this);
         },
         _objPatternMatched:function(evt){
+            var that = this;
             var orderId = evt.getParameter("arguments").OrderId;
+            this.getView().setBusy(true);
             this.getOwnerComponent().getModel().read("/Orders",{
                 urlParameters:{
                     "$expand":"Customer,Order_Details"
                 },
                 filters:[new sap.ui.model.Filter("OrderID","EQ",orderId)],
                 success:function(oData,results){
+                    var model = new sap.ui.model.json.JSONModel(oData.results[0]);
+                    that.getView().setModel(model, "model");
+                    that.getView().setBusy(false);
                     console.log(oData);
                 },
                 error:function(oError){
